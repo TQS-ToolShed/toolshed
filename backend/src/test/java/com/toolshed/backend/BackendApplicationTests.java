6753aa449459a898381c296142853ce9657c6287
Eduardo Rosário <eduardojfrosario@gmail.com>
2025-11-18 12:02:39 +0000

    create base for backend and frontend

---
+++ backend/src/test/java/com/toolshed/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolshed.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
