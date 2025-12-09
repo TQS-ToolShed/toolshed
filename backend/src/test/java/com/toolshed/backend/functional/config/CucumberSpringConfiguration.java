b99cab1f706450a714141b30ffcd37331c037eeb
lopes04 <henriquelopes@ua.pt>
2025-12-09 12:49:48 +0000

    bug fixes

--- backend/src/test/java/com/toolshed/backend/functional/config/CucumberSpringConfiguration.java
+++ backend/src/test/java/com/toolshed/backend/functional/config/CucumberSpringConfiguration.java
@@ -1,12 +1,13 @@
 package com.toolshed.backend.functional.config;
 
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 
 @CucumberContextConfiguration
-@ContextConfiguration(classes = CucumberSpringConfiguration.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
 public class CucumberSpringConfiguration {
-    // This class provides a minimal Spring context configuration for Cucumber tests
-    // We don't need @SpringBootTest here since we're testing against the running application
+    // This class provides Spring context configuration for Cucumber tests
 }
